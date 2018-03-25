6fa5dd3a01983f6358ba5f93730e49f58c9aee2c
ChrisFadden <chris_fadden@yahoo.com>
2018-03-24 20:30:54 -0400

    Added prelim data

---
+++ saveData.py
@@ -0,0 +1,42 @@
+import h5py
+import numpy as np
+import random
+import re
+
+fout = './build/Estatic.h5'
+f5 = h5py.File(fout,"r")
+
+#Node File
+fn = './build/microstrip.msh'
+fp = open(fn,"r")
+
+nflag = False
+eflag = False
+ii = 0
+for line in fp:
+    if("$EndNodes" in line):
+        break 
+    if(eflag):
+        linerg = re.findall('[\d.]+',line)
+        nodes[ii,0] = linerg[1]
+        nodes[ii,1] = linerg[2]
+        nodes[ii,2] = linerg[3]
+        ii = ii+1
+    if(nflag):
+        nflag = False
+        numNodes = int(line)
+        nodes = np.zeros((numNodes,3))
+        eflag = True
+    if("$Nodes" in line):
+        nflag = True
+fp.close()
+
+f5.create_dataset("nodes", data=nodes)
+
+#First File
+fn = './build/Vnode.dat'
+vout = np.loadtxt(fn,skiprows=1,usecols=1)
+
+f5.create_dataset("Vout",data=vout)
+
+
