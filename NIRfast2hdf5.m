1c03eaa61f2681e134f72bd9bf9dcca063c4734b
ChrisFadden <czf41@psu.edu>
2018-04-03 15:11:10 -0400

    Added source and measurement tags

--- NIRfast2hdf5.m
+++ NIRfast2hdf5.m
@@ -29,15 +29,37 @@ hdf5write(f5_path,'Mesh/Elements/ElemType2/Nodes',mesh.elements','WriteMode','ap
 %Create tags for elements
 tags = ones(size(mesh.elements,1),1);
 for ii = 1:length(tags) 
-   tags(ii) = floor(mean([mesh.region(mesh.elements(ii,1));...
+   tags(ii) = mode([mesh.region(mesh.elements(ii,1));...
                     mesh.region(mesh.elements(ii,2));...
-                    mesh.region(mesh.elements(ii,3))]));
+                    mesh.region(mesh.elements(ii,3))]);
     boundary = [mesh.bndvtx(mesh.elements(ii,1)),...
                 mesh.bndvtx(mesh.elements(ii,2)),...
                 mesh.bndvtx(mesh.elements(ii,3))];
     if(any(boundary))
        tags(ii) = 786; 
     end
+    
+    %Sources and Sinks (2D ONLY CURRENTLY)
+    node1 = mesh.nodes(mesh.elements(ii,1),1:2);
+    node2 = mesh.nodes(mesh.elements(ii,2),1:2);
+    node3 = mesh.nodes(mesh.elements(ii,3),1:2);
+    
+    %Sources 
+    for jj = 1:size(mesh.source.coord,1)
+        srcCoord = mesh.source.coord(jj,:);
+        if (all(node1 == srcCoord) || all(node2 == srcCoord) || all(node3 == srcCoord))
+            tags(ii) = 333;
+        end    
+    end
+    
+    %Measurements
+    for jj = 1:size(mesh.meas.coord,1)
+        srcMeas = mesh.meas.coord(jj,:);
+        if (all(node1 == srcMeas) || all(node2 == srcMeas) || all(node3 == srcMeas))
+            tags(ii) = 555;
+        end    
+    end
+           
 end
 hdf5write(f5_path,'Mesh/Elements/ElemType2/Tag',tags,'WriteMode','append');
 
