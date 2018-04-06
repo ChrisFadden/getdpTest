189cfc8c3b5d267c4f762a07fe4e78e798833f0c
ChrisFadden <czf41@psu.edu>
2018-04-06 13:30:50 -0400

    fixed some issues with modular

--- Python/saveData.py
+++ Python/saveData.py
@@ -14,7 +14,6 @@ f5 = h5py.File(fout,"a")
 gpout = f5.create_group("Output")
 
 #Save Data to HDF5
-fn = '../build/Vnode.dat'
 vout = np.loadtxt(fn,skiprows=1,usecols=1)
 gpout.create_dataset("Vout",data=vout)
 
